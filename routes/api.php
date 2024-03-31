2894f0a090906a4853b884bcec43f4b96939ed49
asfiaaiman <aiman.asfia@gmail.com>
2024-03-31 21:03:25 +0500

    Rate-limiting using Throttle

--- routes/api.php
+++ routes/api.php
@@ -9,21 +9,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-Route::apiResource('/authors', AuthorController::class);
-Route::resource('/books', BookController::class)->only([
-    'index', 'store', 'update', 'destroy'
-]);
-Route::resource('/patrons', PatronController::class)->only([
-    'index', 'store', 'update', 'destroy'
-]);
-
-// For searching books
-Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
-Route::get('/authors/{author}/books', [BookController::class, 'fetchBooksByAuthor']);
-
-// Borrow Book
-Route::post('patrons/{patronId}/books/{bookId}/borrow', [PatronController::class, 'borrowBook']);
-
-// Return Book
-Route::post('patrons/{patronId}/books/{bookId}/return', [PatronController::class, 'returnBook']);
-
+Route::middleware('throttle:3,10')->group(function () {
+    Route::apiResource('/authors', AuthorController::class);
+    Route::resource('/books', BookController::class)->only(
+        ['index', 'store', 'update', 'destroy']
+    );
+    Route::resource('/patrons', PatronController::class)->only(
+        ['index', 'store', 'update', 'destroy']
+    );
+    Route::get('/books/search', [BookController::class, 'search'])->name('books.search');
+    Route::get('/authors/{author}/books', [BookController::class, 'fetchBooksByAuthor']);
+    Route::post('patrons/{patronId}/books/{bookId}/borrow', [PatronController::class, 'borrowBook']);
+    Route::post('patrons/{patronId}/books/{bookId}/return', [PatronController::class, 'returnBook']);
+});
